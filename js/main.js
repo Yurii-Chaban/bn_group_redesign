26c727cb8913ae4bd55519500b2bf4c11743fa11
Yuriy Chaban <Yuriy Chaban>
2017-01-16 01:51:10 +0200

    add new 6 pages

--- js/main.js
+++ js/main.js
@@ -21,48 +21,7 @@ $(document).ready(function() {
 		centerPadding: '0',
 	});
 
-	// use video
-	var videoPlayButton,
-	videoWrapper = document.getElementsByClassName('video-wrapper')[0],
-	video = document.getElementsByTagName('video')[0],
-	videoMethods = {
-		renderVideoPlayButton: function() {
-			if (videoWrapper.contains(video)) {
-				this.formatVideoPlayButton()
-				video.classList.add('has-media-controls-hidden')
-				videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
-				videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
-			}
-		},
-
-		formatVideoPlayButton: function() {
-			videoWrapper.insertAdjacentHTML('beforeend', '\
-				<svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
-				<circle cx="100" cy="100" r="90" fill="none" stroke-width="5" stroke="#ffaf13"/>\
-				<polygon points="70, 55 70, 145 145, 100" fill="#ffaf13"/>\
-				</svg>\
-				')
-		},
-
-		hideVideoPlayButton: function() {
-			video.play()
-			videoPlayButton.classList.add('is-hidden')
-			video.classList.remove('has-media-controls-hidden')
-			video.setAttribute('controls', 'controls')
-		}
-	}
-
-	videoMethods.renderVideoPlayButton();
-
-	// hide poster video on click play video
-	$(".video-overlay-play-button").click(function() {
-		$(".poster-img").css({
-			"display" : "none"
-		});
-		$(".video-section .video-wrapper .video-sign").css({
-			"z-index" : "-1"
-		});
-	});
+	
 
 	// for mobile menu submenu
 	$(".sub-menu-mobile > a")['on']("click", function(){
@@ -92,6 +51,45 @@ $(document).ready(function() {
 		$(".voting-form-result-wrapper").css({
 			"display" : "block"
 		});
+	});
+
+	// for press-center slider
+	$('.press-center-more-slider').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: true,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			}
+		}
+		],
+		nextArrow: '<div class="slider-next"><i class="fa fa-angle-right"></i></div>',
+		prevArrow: '<div class="slider-prev"><i class="fa fa-angle-left"></i></div>',
+	});
+
+	// press-center- more page fancybox
+	$(".fancybox").fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
 	});
 	
 });
